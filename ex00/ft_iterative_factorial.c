149059a68bbb739e2adde4eafc5618388873629c
Matthew Gould <matt@everettgould.com>
2016-10-29 11:18:47 -0700

    exercise 00 done

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,24 +6,25 @@
 /*   By: mgould <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/10/29 00:06:49 by mgould            #+#    #+#             */
-/*   Updated: 2016/10/29 00:13:14 by mgould           ###   ########.fr       */
+/*   Updated: 2016/10/29 11:16:55 by mgould           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_iterative_factorial(int nb )
+int	ft_iterative_factorial(int nb)
 {
 	int temp;
 	int i;
 
 	i = 0;
 	temp = 1;
-
+	if (nb > 12)
+	{
+		return (0);
+	}
 	while (i < nb)
 	{
 		temp = temp * nb;
-		i++;
 		nb--;
 	}
-
 	return (temp);
 }
